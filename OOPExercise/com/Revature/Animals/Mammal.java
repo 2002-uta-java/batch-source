db5a7182782d7fe4a179858b167aed3a98bbcca1
Israel Garcia <ingarcia@miners.utep.edu>
2020-02-05 14:42:58 -0600

    Deleted packages

--- OOPExercise/com/Revature/Animals/Mammal.java
+++
@@ -1,9 +0,0 @@
-package com.Revature.Animals;
-public interface Mammal{
-    
-    abstract void giveBirth();
-    
-    default void feedYoungling(){
-        System.out.println("Youngling Fed");
-    }
-}
--- OOPExercise/com/Revature/Animals/Walkable.java
+++
@@ -1,5 +0,0 @@
-package com.Revature.Animals;
-public interface Walkable{
-
-    abstract void walk();
-}
