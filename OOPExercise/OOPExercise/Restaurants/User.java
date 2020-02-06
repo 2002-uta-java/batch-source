f4ea8bf1d744694458eadb0e40b8e81e2fdac8bf
vyklimavicius <vyklimavicius@gmail.com>
2020-02-05 20:36:06 -0600

    equals() method

--- OOPExercise/OOPExercise/Restaurants/User.java
+++ OOPExercise/OOPExercise/Restaurants/User.java
@@ -4,6 +4,7 @@ public class User{
 
     String name;
 
+    //Constructor
     public User(String name){
         this.name = name;
     }
@@ -12,13 +13,22 @@ public class User{
         throw new MyCustomException("My exception");
     }
 
+    //equals() method of Object class
+    public static boolean checkName(String name){
+        return name.equals(name);
+    }
+
     public static void main(String[] args){
+        User vytautas = new User("Vytautas");
         Waiter carlos = new Waiter();
+        String userName = vytautas.name;
 
         System.out.println(carlos.restaurantName);
         System.out.println(carlos.specialty);
         System.out.println(carlos.lunchValue);
         System.out.println(carlos.dinnerValue);
+
+        System.out.println(checkName(userName));
 
         try{
             handleException();
