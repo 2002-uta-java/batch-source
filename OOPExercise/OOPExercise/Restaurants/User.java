3e13e654c65307ef5a7eed3a23f9243364302bf1
vyklimavicius <vyklimavicius@gmail.com>
2020-02-05 21:56:13 -0600

    hashCode() method

--- OOPExercise/OOPExercise/Restaurants/User.java
+++ OOPExercise/OOPExercise/Restaurants/User.java
@@ -3,32 +3,62 @@ package OOPExercise.OOPExercise.Restaurants;
 public class User{
 
     String name;
+    //creating id to replace default hashcode of Object
+    int id;
 
     //Constructor
-    public User(String name){
+    public User(String name, int id){
+        super();
         this.name = name;
+        this.id = id;
     }
 
     public static void handleException() throws MyCustomException{
         throw new MyCustomException("My exception");
     }
 
-    //equals() method of Object class
-    public static boolean checkName(String name){
-        return name.equals(name);
+    //equals() method from Object 
+    @Override
+    public boolean equals(Object obj){
+        
+        if(this.getClass() == obj.getClass()){
+            return true;
+        } else {
+            return false;
+        }
+        
+    }
+
+    //hashCode() method from Object
+    @Override
+    public int hashCode(){
+        return this.id;
     }
 
     public static void main(String[] args){
-        User vytautas = new User("Vytautas");
+        User vytautas = new User("Vytautas", 0);
+        User klimavicius = new User("Klimavicius", 0);
         Waiter carlos = new Waiter();
-        String userName = vytautas.name;
+        
+        //Compare Objects to use equal() and hashCode()
+        if(vytautas.hashCode() == klimavicius.hashCode()){
+            System.out.println("Both objects are equal");
+        }
+
+        if(vytautas.equals(klimavicius)){
+            System.out.println("Both objects are equal");
+        }
 
         System.out.println(carlos.restaurantName);
         System.out.println(carlos.specialty);
         System.out.println(carlos.lunchValue);
         System.out.println(carlos.dinnerValue);
+        // System.out.println(vytautas.equals(klimavicius));
 
-        System.out.println(checkName(userName));
+        // System.out.println(vytautas.getClass());
+        // System.out.println(klimavicius instanceof User);
+        // System.out.println(vytautas.hashCode());
+        // System.out.println(klimavicius.hashCode());
 
         try{
             handleException();
