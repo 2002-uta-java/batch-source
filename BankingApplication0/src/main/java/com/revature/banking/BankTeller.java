86216f888d8fea677128b25df92c2061cf903538
Jared F Bennatt <bennattj@gmail.com>
2020-02-12 10:21:47 -0600

    Starting writing BankingApplication. Finishing up loginPrompt method. Saving in case it's my laptop that's on fire.

--- BankingApplication0/src/main/java/com/revature/banking/BankTeller.java
+++ BankingApplication0/src/main/java/com/revature/banking/BankTeller.java
@@ -1,6 +1,9 @@
 package com.revature.banking;
 
 public abstract class BankTeller {
+	public static final int USER_LOGIN = 0;
+	public static final int CREATE_ACCOUNT = 1;
+
 	/**
 	 * Performs the requested transaction with the given string arguments.
 	 * 
@@ -15,8 +18,14 @@ public abstract class BankTeller {
 		switch (type) {
 		case USER_LOGIN:
 			userLogin(args);
+			break;
+		case CREATE_ACCOUNT:
+			createAccount(args);
+			break;
 		}
 	}
+
+	protected abstract void createAccount(String[] args) throws TransactionException;
 
 	protected abstract void userLogin(String... args) throws TransactionException;
 }
--- BankingApplication0/src/main/java/com/revature/banking/application/BankingApplication.java
+++ BankingApplication0/src/main/java/com/revature/banking/application/BankingApplication.java
@@ -3,10 +3,28 @@ package com.revature.banking.application;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.regex.Pattern;
 
 import com.revature.banking.BankTeller;
 
 public class BankingApplication {
+	/**
+	 * This is used so that when I read a response I can always return an integer
+	 * (all choices, of what action to take, will be a integer choice).
+	 */
+	private static final int EXIT_RESPONSE = 0;
+	private static final String EXIT_STRING = "exit";
+
+	private static final int LOGIN = 1;
+	private static final int CREATE_LOGIN = 2;
+	private static final int LOGOUT = 3;
+
+	/**
+	 * This is solely used to validate input so that I avoid a
+	 * NumberFormatException. The program itself will decide whether or not the
+	 * integer choice is valid.
+	 */
+	private static final Pattern INTEGER_PATTERN = Pattern.compile("\\d+");
 
 	private final BankTeller teller;
 	private final BufferedReader input;
@@ -17,17 +35,56 @@ public class BankingApplication {
 	}
 
 	public void start() {
-		boolean exit = false;
-		while (!exit) {
-			boolean logout = false;
-			exit = loginPrompt();
-			while (!exit && !logout) {
+		// keep going until
+		while (true) {
 
-			}
 		}
 	}
 
-	private boolean loginPrompt() {
-		System.out.println("What would you like to do? (please type 1 or 2)");
+	private int loginPrompt() {
+		System.out
+				.println("What would you like to do? Please type 1 or 2 (you may exit at any time by typing \"exit\"");
+		System.out.println("1) Login to an existing account");
+		System.out.println("2) Sign up and create a new account");
+		try {
+			final int response = readChoice();
+
+			switch (response) {
+			case EXIT_RESPONSE:
+				return EXIT_RESPONSE;
+			case 1:
+				return LOGIN;
+			case 2:
+				return CREATE_LOGIN;
+			// the default is that the user typed an integer but wasn't 1 or 2 therefore
+			// tell the user to type 1 or 2 and re-prompt
+			default:
+				System.out.println(response + " wasn't an option, please try again.");
+				return loginPrompt();
+			}
+		} catch (IllegalChoiceException e) {
+			System.out.println(
+					"Please choose 1,2, or \"exit\". You typed: \"" + e.getMessage() + "\"; please try again.");
+			return loginPrompt();
+		}
+
+	}
+
+	private int readChoice() throws IllegalChoiceException {
+		String line;
+		try {
+			line = input.readLine();
+		} catch (IOException e) {
+			// TODO Eventually should remove this stack trace for user (hopefully this never
+			// happens
+			e.printStackTrace();
+			
+		}
+		if (line.trim().toLowerCase().equals(EXIT_STRING))
+			return EXIT_RESPONSE;
+		if (INTEGER_PATTERN.matcher(line).matches())
+			return Integer.parseInt(line);
+
+		throw new IllegalChoiceException(line);
 	}
 }
