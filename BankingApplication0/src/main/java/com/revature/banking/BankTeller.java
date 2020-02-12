fe3c6229ffd1fb1ac90f59b374e0905cce3a3b00
Jared F Bennatt <bennattj@gmail.com>
2020-02-12 15:35:47 -0600

    Started adding addFunds method in BankTeller abstract class and BankApplication.

--- BankingApplication0/src/main/java/com/revature/banking/BankTeller.java
+++ BankingApplication0/src/main/java/com/revature/banking/BankTeller.java
@@ -1,8 +1,12 @@
 package com.revature.banking;
 
+import java.util.List;
+
 public abstract class BankTeller {
 	public static final int USER_LOGIN = 0;
 	public static final int CREATE_ACCOUNT = 1;
+	public static final int VIEW_ACCOUNTS = 2;
+	public static final int ADD_FUNDS = 3;
 
 	/**
 	 * Performs the requested transaction with the given string arguments.
@@ -10,22 +14,46 @@ public abstract class BankTeller {
 	 * @param transaction Integer representing the type of transaction to be
 	 *                    performed
 	 * @param args        The arguments needed to perform the given transaction.
+	 * @return Returns a list of strings as the "output" (if there is any).
 	 * @throws TransactionException Thrown if the transaction cannot be performed. A
 	 *                              message should be included explaining why the
 	 *                              transaction failed.
 	 */
-	public void doTransaction(int type, String... args) throws TransactionException {
+	public List<String> doTransaction(int type, String... args) throws TransactionException {
 		switch (type) {
 		case USER_LOGIN:
-			userLogin(args);
-			break;
+			return userLogin(args);
 		case CREATE_ACCOUNT:
-			createAccount(args);
-			break;
+			return createAccount(args);
+		case ADD_FUNDS:
+			return addFunds(Integer.parseInt(args[0]), Double.parseDouble(args[1]));
+		case VIEW_ACCOUNTS:
+			return viewAccounts();
+		default:
+			throw new TransactionException("This operation isn't supported");
 		}
 	}
 
-	protected abstract void createAccount(String[] args) throws TransactionException;
+	/**
+	 * This method should return a list of Strings that represent the state of the
+	 * currently logged on user's accounts.
+	 * 
+	 * @return
+	 */
+	protected abstract List<String> viewAccounts();
 
-	protected abstract void userLogin(String... args) throws TransactionException;
+	/**
+	 * This should add the amount specified to the account specified for this user.
+	 * 
+	 * @param account The index of the account (for this user) to add funds to.
+	 * @param amount  The amount to add to the specified account.
+	 * @return Returns a singleton list containing the information for the modified
+	 *         account.
+	 * @throws TransactionException
+	 */
+	protected abstract List<String> addFunds(int account, final double amount) throws TransactionException;
+
+	protected abstract List<String> createAccount(String[] args) throws TransactionException;
+
+	protected abstract List<String> userLogin(String[] args) throws TransactionException;
 }
--- BankingApplication0/src/main/java/com/revature/banking/application/BankingApplication.java
+++ BankingApplication0/src/main/java/com/revature/banking/application/BankingApplication.java
@@ -3,12 +3,12 @@ package com.revature.banking.application;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.List;
 import java.util.TreeMap;
 import java.util.regex.Pattern;
 
 import com.revature.banking.BankTeller;
+import com.revature.banking.TransactionException;
 
 public class BankingApplication {
 	/**
@@ -21,16 +21,23 @@ public class BankingApplication {
 	private static final int LOGOUT_RESPONSE = -1;
 	private static final String LOGOUT_STRING = "logout";
 
+	private static final int YES_RESPONSE = 1;
+	private static final int NO_RESPONSE = 2;
+	private static final String YES_STRING = "y";
+	private static final String NO_STRING = "n";
+
 	private static final int LOGIN = 1;
 	private static final int CREATE_LOGIN = 2;
 
-	private static final int ADD_FUNDS = 1;
-	private static final int WITHDRAW_FUNDS = 2;
-	private static final int OPEN_NEW_ACCOUNT = 3;
-	private static final int REQUEST_FUNDS = 6;
-	private static final int TRANSFER_FUNDS = 5;
-	private static final int CLOSE_ACCOUNT = 4;
+	private static final int VIEW_FUNDS = 1;
+	private static final int ADD_FUNDS = 2;
+	private static final int WITHDRAW_FUNDS = 3;
+	private static final int OPEN_NEW_ACCOUNT = 4;
+	private static final int CLOSE_ACCOUNT = 5;
+	private static final int TRANSFER_FUNDS = 6;
+	private static final int REQUEST_FUNDS = 7;
 
+	private static final String VIEW_FUNDS_PROMPT = "View funds.";
 	private static final String ADD_FUNDS_PROMPT = "Add funds.";
 	private static final String WITHDRAW_FUNDS_PROMPT = "Withdraw Funds.";
 	private static final String OPEN_NEW_ACCOUNT_PROMPT = "Open new account";
@@ -44,6 +51,7 @@ public class BankingApplication {
 	private static final TreeMap<Integer, String> TRANSACTION_PROMPTS = new TreeMap<>();
 
 	static {
+		TRANSACTION_PROMPTS.put(VIEW_FUNDS, VIEW_FUNDS_PROMPT);
 		TRANSACTION_PROMPTS.put(ADD_FUNDS, ADD_FUNDS_PROMPT);
 		TRANSACTION_PROMPTS.put(WITHDRAW_FUNDS, WITHDRAW_FUNDS_PROMPT);
 		TRANSACTION_PROMPTS.put(OPEN_NEW_ACCOUNT, OPEN_NEW_ACCOUNT_PROMPT);
@@ -57,7 +65,16 @@ public class BankingApplication {
 	 * NumberFormatException. The program itself will decide whether or not the
 	 * integer choice is valid.
 	 */
-	private static final Pattern INTEGER_PATTERN = Pattern.compile("\\d+");
+	private static final Pattern INTEGER_PATTERN = Pattern.compile("^\\d+$");
+
+	/**
+	 * Regex for currency. Must have at least one digit to the left of decimal. Then
+	 * the decimal can be there or not, if so, there must be exatly one decimal
+	 * point followed by 1 or 2 digits.
+	 * 
+	 * I allow this to be negative (the Bank will throw an exception if it is)
+	 */
+	private static final Pattern DECIMAL_PATTERN = Pattern.compile("-?\\d+(\\.\\d{1,2})?");
 
 	private final BankTeller teller;
 	private final BufferedReader input;
@@ -113,6 +130,9 @@ public class BankingApplication {
 						System.out.println();
 						System.out.println();
 						continue begin_prompt;
+					case VIEW_FUNDS:
+						viewFundsPrompt();
+						break;
 					case ADD_FUNDS:
 						addFundsPrompt();
 						break;
@@ -155,6 +175,17 @@ public class BankingApplication {
 		}
 	}
 
+	private void viewFundsPrompt() {
+		try {
+			final List<String> accounts = teller.doTransaction(BankTeller.VIEW_ACCOUNTS);
+			System.out.println("Accounts:");
+			for (final String account : accounts)
+				System.out.println('\t' + account);
+		} catch (TransactionException e) {
+			System.out.println("Unable to view your accounts: " + e.getMessage());
+		}
+	}
+
 	private void closeAccountPrompt() {
 		// TODO Auto-generated method stub
 
@@ -181,8 +212,62 @@ public class BankingApplication {
 	}
 
 	private void addFundsPrompt() {
-		// TODO Auto-generated method stub
+		// keep trying until they add funds or don't respond with "y" when asked to try
+		// again.
+		while (true) {
+			try {
+				// get the accounts
+				final List<String> accounts = teller.doTransaction(BankTeller.VIEW_ACCOUNTS);
+				int numAccounts = 0;
+				int choice = 1;// we're going to subtract 1 from choice, if there is only 1 account that would
+								// have to be the "first" account
 
+				// if there are more than one accounts, display them all and let the user choose
+				// which one to place money into
+				if (accounts.size() > 1) {
+					System.out.println("Please choose an account to add funds to (1, 2, etc.):");
+
+					for (final String account : accounts) {
+						System.out.println("\t" + ++numAccounts + ") " + account);
+					}
+
+					choice = readChoice();
+
+					if (choice < 1 || choice > numAccounts)
+						throw new IllegalArgumentException("You must choose a number from 1 to " + numAccounts);
+
+					// send back the index of the account to add to (for this user). This will be
+					// zero indexed, so subtract one from choice
+				} else {
+					// display their one account for them
+					System.out.println('\t' + accounts.get(0));
+				}
+
+				System.out.println("How much would you like to add (please entire a decimal or integer amount)? ");
+				final double amount = readAmount();
+
+				// give the teller the index of the account to add to (choice - 1) and the
+				// amount to add (both as strings).
+				final List<String> updatedAccount = teller.doTransaction(BankTeller.ADD_FUNDS, "" + (choice - 1),
+						"" + amount);
+				System.out.println("New Balance:");
+				System.out.println('\t' + updatedAccount.get(0));
+			} catch (TransactionException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (IllegalChoiceException e) {
+				System.out.println(e.getMessage());
+				System.out.println("Would you like to try again?(y/n)");
+				try {
+					final int choice = readChoice();
+					if (choice != YES_RESPONSE)
+						return;// exit from trying to add funds
+				} catch (IllegalChoiceException e1) {
+					// don't care what they typed if it wasn't "y"
+					return;// stop trying to add funds
+				}
+			}
+		}
 	}
 
 	private void loginPrompt() {
@@ -196,40 +281,47 @@ public class BankingApplication {
 	}
 
 	private int beginningPrompt() {
-		System.out.println("What would you like to do? Please type 1 or 2 (you may exit at any time by typing \""
-				+ EXIT_STRING + "\")");
-		System.out.println("1) Login to an existing account");
-		System.out.println("2) Sign up and create a new account");
-		try {
-			final int response = readChoice();
+		// the only way out of this loop is to give a valid input (i.e. 1, 2, or exit)
+		while (true) {
+			System.out.println("What would you like to do? Please type 1 or 2 (you may exit at any time by typing \""
+					+ EXIT_STRING + "\")");
+			System.out.println("1) Login to an existing account");
+			System.out.println("2) Sign up and create a new account");
+			try {
+				final int response = readChoice();
 
-			switch (response) {
-			case EXIT_RESPONSE:
-				return EXIT_RESPONSE;
-			case 1:
-				return LOGIN;
-			case 2:
-				return CREATE_LOGIN;
-			case LOGOUT_RESPONSE:
-				System.out.println("You aren't logged in. Please select again.");
-				return beginningPrompt();
-			// the default is that the user typed an integer but wasn't 1 or 2 therefore
-			// tell the user to type 1 or 2 and re-prompt
-			default:
-				System.out.println(response + " wasn't an option, please try again.");
-				return beginningPrompt();
+				switch (response) {
+				case EXIT_RESPONSE:
+					return EXIT_RESPONSE;
+				case 1:
+					return LOGIN;
+				case 2:
+					return CREATE_LOGIN;
+				case LOGOUT_RESPONSE:
+					System.out.println("You aren't logged in. Please select again.");
+					System.out.println();
+					System.out.println();
+					break;
+				// the default is that the user typed an integer but wasn't 1 or 2 therefore
+				// tell the user to type 1 or 2 and re-prompt
+				default:
+					System.out.println(response + " wasn't an option, please try again.");
+					System.out.println();
+					System.out.println();
+				}
+			} catch (IllegalChoiceException e) {
+				System.out.println(
+						"Please choose 1,2, or \"exit\". You typed: \"" + e.getMessage() + "\"; please try again.");
+				System.out.println();
+				System.out.println();
 			}
-		} catch (IllegalChoiceException e) {
-			System.out.println(
-					"Please choose 1,2, or \"exit\". You typed: \"" + e.getMessage() + "\"; please try again.");
-			return beginningPrompt();
 		}
 
 	}
 
 	private String readLine() {
 		try {
-			return input.readLine();
+			return input.readLine().trim();
 		} catch (IOException e) {
 			System.out.println(
 					"Could not read your input and the program is now exiting (if program does not exit, try pressing ctrl-c). If you continue to have this issue please copy the error message below and send to it to a developer.");
@@ -239,12 +331,29 @@ public class BankingApplication {
 		}
 	}
 
+	private double readAmount() throws IllegalChoiceException {
+		final String line = readLine();
+
+		if (DECIMAL_PATTERN.matcher(line).matches())
+			return Double.parseDouble(line);
+
+		// else throw exception
+		throw new IllegalChoiceException(line + " isn't a valid amount");
+	}
+
 	private int readChoice() throws IllegalChoiceException {
-		String line = readLine();
-		if (line.trim().equalsIgnoreCase(EXIT_STRING))
+		String line = readLine().toLowerCase();
+
+		switch (line) {
+		case EXIT_STRING:
 			return EXIT_RESPONSE;
-		if (line.trim().equalsIgnoreCase(LOGOUT_STRING))
+		case LOGOUT_STRING:
 			return LOGOUT_RESPONSE;
+		case NO_STRING:
+			return NO_RESPONSE;
+		case YES_STRING:
+			return YES_RESPONSE;
+		}
 
 		if (INTEGER_PATTERN.matcher(line).matches())
 			return Integer.parseInt(line);
