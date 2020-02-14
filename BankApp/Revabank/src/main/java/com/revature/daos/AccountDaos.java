de8910904f88da33f067e31994700eee3b62ba4d
Israel Garcia <ingarcia@miners.utep.edu>
2020-02-14 14:54:03 -0600

    Interface done

--- BankApp/Revabank/src/main/java/com/revature/daos/AccountDaos.java
+++ BankApp/Revabank/src/main/java/com/revature/daos/AccountDaos.java
@@ -1,6 +1,16 @@
 package com.revature.daos;
 
-public interface AccountDaos {
+import java.util.List;
 
+import com.revature.models.Account;
+
+public interface AccountDaos {
 	
+	//This will be implemented once I get multiple accounts
+	public List<Account> getAccounts();
+	
+	public Account getAccount(int id);
+	public int createAccount(Account a);
+	public int updateAccount(Account a);
+	public int deleteAccount(Account a);
 }
--- BankApp/Revabank/src/main/java/com/revature/daos/UserDaos.java
+++ BankApp/Revabank/src/main/java/com/revature/daos/UserDaos.java
@@ -1,5 +1,12 @@
 package com.revature.daos;
 
+import java.util.List;
+
+import com.revature.models.User;
+
 public interface UserDaos {
-	
+	public List<User> getUsers();
+	public User getUserById(int id);
+	public boolean createUser(User u);
+	public boolean deleteUser(User u);
 }
