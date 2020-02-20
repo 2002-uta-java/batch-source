b620e6e09becfb34d5c382cb2f0a450dc493fee0
wheelerkeith <nemimimonkey@gmail.com>
2020-02-19 19:56:09 -0600

    Added Project 1

---
+++ ers/src/main/java/com/revature/ers/App.java
@@ -0,0 +1,81 @@
+package com.revature.ers;
+
+/**
+Expense Reimbursement System (ERS)
+
+Project 1 will be an individual project based off of an employee expense reimbursement system (or similar*). 
+It will be a full stack application utilizing HTML, CSS, and JavaScript on the frontend and a 
+Java application on the backend. This Java application will utilize Servlets to process user requests 
+from the front end. The following user stories are required of the application:
+
+-An Employee can login
+-An Employee can view the Employee Homepage
+-An Employee can logout
+-An Employee can submit a reimbursement requestf                                                                                                                                                                                                                                                                                                                                      xx
+-An Employee can view their pending reimbursement requestsl
+-An Employee can view their resolved reimbursement requests
+-An Employee can view their information (profile)’
+-An Employee can update their information 
+
+-A Manager can login
+-A Manager can view the Manager Homepage
+-A Manager can logout
+-A Manager can approve/deny pending reimbursement requests
+-A Manager can view all pending requests from all employees
+-A Manager can view all resolved requests from all employees and see which manager resolved it
+-A Manager can view all Employees    
+-A Manager can view reimbursement requests from a single Employee
+ 
+
+
+Additional Requirements:
+for testing application code
+Ensuring code quality with a static code analysis
+Logging using log4j or a similar logging framework
+JUnit 
+
+
+Optional User Stories:
+------------------------------------------------------------------
+-An Employee can upload an image of his/her receipt as part of the reimbursement request 
+-A Manager can view images of the receipts from reimbursement requests
+-An Employee receives an email when one of their reimbursement requests is resolved 
+-A Manager can register an Employee, which sends the Employee an email with their username  and temp password 
+-An Employee can reset their password
+-Password encryption
+-Including visual aids, such as charts showing spending categories or spending over time 
+
+
+Technologies:
+------------------------------------------------------------------
+-Java 1.8
+-Servlets
+-JDBC
+-SQL
+-HTML
+-CSS
+-Javascript
+-Bootstrap
+-AJAX
+-JUnit
+-log4j 
+
+Environment:
+------------------------------------------------------------------
+-Tomcat Server
+-PostgreSQL Database
+-Eclipse/Spring Tools Suite
+-DBeaver
+
+
+It is possible to do a variation of the ERS system if you’d like. 
+If you have a similar idea that you would like to implement, it can be discussed 
+with your trainer, as long as there are equivalent user stories that can be defined
+*/
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
