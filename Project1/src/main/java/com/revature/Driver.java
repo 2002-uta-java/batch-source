4ea62f9089933526d04322bf2cfd0047c30c2d02
Nicholas Desmornes <ndesmorn@andrew.cmu.edu>
2020-02-27 15:13:37 -0600

    employee, manager

--- Project1/src/main/java/com/revature/Driver.java
+++ Project1/src/main/java/com/revature/Driver.java
@@ -9,6 +9,7 @@ public class Driver {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
 		try {
 			Connection c = ConnectionUtil.getConnection();
 			String driverName = c.getMetaData().getDriverName();
--- Project1/src/main/java/com/revature/models/Employee.java
+++ Project1/src/main/java/com/revature/models/Employee.java
@@ -1,5 +1,20 @@
 package com.revature.models;
 
 public class Employee {
-
+	
+	private String email;
+	private String firstName;
+	private String lastName;
+	private String gender;
+	private String password;
+	
+	public Employee(String email, String firstName, String lastName, String gender, String password) {
+		this.email = email;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.gender = gender;
+		this.password = password;
+	}
+	
+	
 }
