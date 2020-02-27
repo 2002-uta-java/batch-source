eea33405f8bb8c41931b8b6b449eed6196cc6028
Nicholas Desmornes <ndesmorn@andrew.cmu.edu>
2020-02-27 14:14:59 -0600

    Added connection.

--- Project1/src/main/java/com/revature/Driver.java
+++ Project1/src/main/java/com/revature/Driver.java
@@ -1,10 +1,21 @@
 package com.revature;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import com.revature.util.ConnectionUtil;
+
 public class Driver {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		try {
+			Connection c = ConnectionUtil.getConnection();
+			String driverName = c.getMetaData().getDriverName();
+			System.out.println(driverName);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
 	}
 
 }
