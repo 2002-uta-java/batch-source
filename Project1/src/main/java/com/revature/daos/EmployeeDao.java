c7fb7df3329118d3b294c1c74e8383ae68d1b294
Nicholas Desmornes <ndesmorn@andrew.cmu.edu>
2020-02-28 16:11:09 -0600

    html/js

--- Project1/src/main/java/com/revature/daos/EmployeeDao.java
+++ Project1/src/main/java/com/revature/daos/EmployeeDao.java
@@ -3,15 +3,12 @@ package com.revature.daos;
 import java.util.List;
 
 import com.revature.models.Employee;
-import com.revature.models.Reimbursement;
 
 public interface EmployeeDao {
 
 	public List<Employee> getEmployees();
 	public Employee getEmployeeById(int id);
-	
 	public void createEmployee(Employee e);
-	
 	public void updateEmployee(Employee e);
 	
 }
--- Project1/src/main/java/com/revature/daos/ReimbursementDao.java
+++ Project1/src/main/java/com/revature/daos/ReimbursementDao.java
@@ -11,4 +11,5 @@ public interface ReimbursementDao {
 	public List<Reimbursement> getReimbursementsByEmployee(Employee e);
 	public void createReimbursement(Reimbursement r);
 	public void updateReimbursement(Reimbursement r, Employee e);
+	
 }
