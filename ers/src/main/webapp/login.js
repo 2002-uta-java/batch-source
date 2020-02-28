442e598c346185f297bdae86ddceb5298396ab3a
Keith Wheeler <nemimimonkey@gmail.com>
2020-02-27 21:49:37 -0600

    added login and register js files

---
+++ ers/src/main/webapp/login.js
@@ -0,0 +1,28 @@
+/**
+ * 
+ */
+
+document.getElementById("login-btn").addEventListener("click", requestLogin);
+
+function requestLogin() {
+    let user = document.getElementById("username").value;
+    let pass = document.getElementById("password").value;
+
+    let xhr = new XMLHttpRequest();
+    let url = "";
+    xhr.open("POST", url);
+
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState == 4 && xhr.status == 200) {
+            let auth = xhr.getResponseHeader("Authorization");
+            sessionStorage.setItem("token", auth);
+            window.location.href="";
+        } else if (xhr.readyState == 4) {
+            console.log("Incorrect Credentials");
+        }
+    }
+
+    xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+    let requestBody = `username=${user}&password=${pass}`;
+    xhr.send(requestBody);
+}
