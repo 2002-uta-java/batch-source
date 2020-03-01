76255508f6d20ea4d532daed4c8f6e5c86d05be3
Nicholas Desmornes <ndesmorn@andrew.cmu.edu>
2020-03-01 12:20:26 -0600

    submit-profile stuff

--- Project1/src/main/webapp/static/Views/HomeManager.js
+++ Project1/src/main/webapp/static/Views/HomeManager.js
@@ -27,7 +27,6 @@ function sendAjaxGet(url, callback, token){
 			callback(this);
 		} else if (this.readyState===4){
             console.log("Ajax failure.")
-			// window.location.href="http://localhost:8080/Project1/login";
 		}
 	}
 	xhr.setRequestHeader("Authorization", token);
@@ -42,7 +41,12 @@ function loadPage(xhr){
     document.getElementById("profile-name").innerHTML = ` ${user.firstName} ${user.lastName} `;
 
     // Load profile information (view profile button).
-    // TODO: (id=profile-body) (model-name, etc.)
+    document.getElementById("modal-name").innerHTML = `${user.firstName} ${user.lastName}`;
+    document.getElementById("modal-email").innerHTML = `${user.email}`;
+    document.getElementById("modal-position").innerHTML = `${user.position}`;
+    document.getElementById("modal-gender").innerHTML = `${user.gender}`;
+    document.getElementById("modal-id").innerHTML = `${user.id}`;
+    // TODO: picture depending on gender (id=)
 }
 
 // Remove user token and return to the login menu.
@@ -51,22 +55,33 @@ function logout() {
     window.location.href="http://localhost:8080/Project1/login";
 }
 
-// Update profile information based on form input.
+// Update profile information based on form input AND re-build page.
 function updateProfile() {
     let formInfo = document.getElementById("edit-profile-form");
-    let fName = formInfo.elements[0].value;
-    let lName = formInfo.elements[1].value;
-    let email = formInfo.elements[2].value;
-    let gender = formInfo.elements[3].value;
+    let new_fName = formInfo.elements[0].value;
+    let new_lName = formInfo.elements[1].value;
+    let new_email = formInfo.elements[2].value;
+    let new_gender = formInfo.elements[3].value;
 
-    console.log(fName);
-    console.log(lName);
-    console.log(email);
-    console.log(gender);
-    
-    // TODO: gather all data (with proper validations) and send a POST request to update the database.
-    // then update the profile information (should not have to reload page).
-    document.getElementById("edit-profile-form-status").innerHTML = Profile successfully updated!;
+    // TODO: gather all data (null arguments handled in backend) and send a POST request to update the database.
+    let updateInfo = {firstName: new_fName, lastName: new_lName, email: new_email, gender: new_gender};
+    let myJSON = JSON.stringify(updateInfo);
+    sendAjaxPost(?, checkToken, myJSON) // sends post, and immediately re-builds page (checkToken).
+    document.getElementById("edit-profile-form-status").innerHTML = "Profile successfully updated!";
+}
+
+// updateProfile AJAX helper. 
+function sendAjaxPost(url, callback, data){
+	let xhr = new XMLHttpRequest();
+	xhr.open("POST", url);
+	xhr.onreadystatechange = function(){
+		if(this.readyState===4 && this.status===200){
+			callback(this);
+		} else if (this.readyState===4){
+            console.log("Ajax failure.")
+		}
+	}
+	xhr.send("data i want to send");
 }
 
 // TODO: function that handles viewing/editing your profile.
