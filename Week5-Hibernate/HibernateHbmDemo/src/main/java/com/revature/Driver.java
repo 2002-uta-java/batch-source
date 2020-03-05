ade68e7e51f2dee83207a549ce61940e762b5171
carolynrehm <carolynhrehm@gmail.com>
2020-03-05 16:16:27 -0600

    hibernate hbm example

---
+++ Week5-Hibernate/HibernateHbmDemo/src/main/java/com/revature/Driver.java
@@ -0,0 +1,14 @@
+package com.revature;
+
+import org.hibernate.Session;
+
+import com.revature.util.HibernateUtil;
+
+public class Driver {
+
+	public static void main(String[] args) {
+		Session s = HibernateUtil.getSession();
+	}
+	
+	
+}
