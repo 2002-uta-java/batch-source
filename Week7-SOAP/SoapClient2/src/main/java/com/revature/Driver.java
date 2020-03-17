9399130896757d5031ef85bc0cd82e80b2133d00
carolynrehm <carolynhrehm@gmail.com>
2020-03-17 14:08:00 -0500

    adding wsimport command

--- Week7-SOAP/SoapClient2/src/main/java/com/revature/Driver.java
+++ Week7-SOAP/SoapClient2/src/main/java/com/revature/Driver.java
@@ -7,6 +7,10 @@ import com.revature.services.EmployeeService;
 import com.revature.services.EmployeeServiceImplService;
 
 public class Driver {
+	
+	/*
+	 *  wsimport -Xnocompile http://localhost:8080/SoapService/employees?wsdl
+	 */
 
 	public static void main(String[] args) {
 		EmployeeServiceImplService employeeServiceImplService = new EmployeeServiceImplService();
