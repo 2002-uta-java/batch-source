eb8fc0164a1b57b90eb7f03a9d20886a33ae852a
Keith Wheeler <nemimimonkey@gmail.com>
2020-03-21 00:04:57 -0500

    heroes tour

--- FirstAngular/tour-of-heroes/src/app/heroes/heroes.component.ts
+++ FirstAngular/tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HEROES } from '../mock-heroes';
 import { Hero } from '../hero';
 
 @Component({
@@ -6,18 +7,18 @@ import { Hero } from '../hero';
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css']
 })
-
 export class HeroesComponent implements OnInit {
 
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm'
-  };
-
+  heroes = HEROES;
+  selectedHero: Hero;
 
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnInit() {
+  }
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
   }
 
 }
